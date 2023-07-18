b8eba244bc515630550fe40153fac494dfc0823f
Gager <mohd.faiz.razwi@gmail.com>
2023-07-18 15:00:15 +0530

    added pratice file

---
+++ practise.js
@@ -0,0 +1,73 @@
+let myLibrary =[];
+
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+}
+
+function addBookToLibrary(){
+let title = document.querySelector("#title").value;
+let author = document.querySelector("#author").value;
+let pages = document.querySelector("#pages").value;
+let read = document.querySelector("#read").checked;
+
+let newBook = new Book(title, author, pages, read);
+myLibrary.push(newBook);
+
+render();
+
+}
+
+function render(){
+let libraryEl = document.querySelector("#library");
+libraryEl.innerHTML ="";
+for ( let i =0; i < myLibrary.length; i++){
+let book = myLibrary[i];
+let bookEl = document.createElement("div");
+
+console.log(book.author);
+
+bookEl.innerHTML = `<div class="card-header">
+    <h3 class="title">${book.title}</h3> 
+    <h5 class="author">${book.author} </h5>
+    </div>
+    <div class="card-body">
+    <p class="pages">${book.pages} Pages</p>
+    <p class="read-status">${book.read ? "Read" : "Not Read Yet"}</p>
+    <button class="remoce-btn" onclick="removeBook(${i})">Remove</button>
+    <button class="toggle-read-btn" onclick="toggleRead(${i})">Toggle Read</button>
+
+</div>`;
+libraryEl.appendChild(bookEl);
+
+}
+}
+
+let newBookbtn = document.querySelector("#new-book-btn");
+newBookbtn.addEventListener("click", function(){
+    let newBookForm =document.querySelector("#new-book-form");
+    newBookForm.style.display = "block";
+})
+
+document.querySelector("#new-book-form").addEventListener("submit", function(event){
+    event.preventDefault();
+    addBookToLibrary();
+    render();
+})
+
+Book.prototype.toggleRead = function(){
+    this.read = !this.read;
+}
+
+function toggleRead(index){
+    myLibrary[index].toggleRead();
+    render();
+}
+
+function removeBook(index){
+    myLibrary.splice(index,1);
+    render();
+}
